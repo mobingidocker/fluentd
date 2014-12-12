2dc93435fe05332c8e855d797f1108ffa37f9254
David Siaw (via Travis CI) <davidsiaw@gmail.com>
2014-12-12 19:48:36 +0900

    remove hardcode, use script to generate starters

---
+++ generatescripts.rb
@@ -0,0 +1,83 @@
+require 'shellwords'
+
+logFiles = [
+	{:tag => "modaemon.log", :location => "/var/log/modaemon/modaemon"},
+	{:tag => "apache.access", :location => "/var/log/container/apache2/access.log"},
+	{:tag => "apache.error", :location => "/var/log/container/apache2/error.log"},
+	{:tag => "supervisor.log", :location => "/var/log/container/supervisor/supervisord.log"}
+]
+
+sourceString = ""
+matchString = ""
+
+logFiles.each do |file|
+
+	sourceString += <<-ENDSOURCE
+<source>
+  type tail
+  format none
+  path #{file[:location]}
+  pos_file /var/log/fluentd/#{file[:tag]}.pos
+  tag #{file[:tag]}
+  read_from_head true
+</source>
+
+	ENDSOURCE
+
+	matchString += <<-ENDMATCH
+<match #{file[:tag]}>
+	type cloudwatch_logs
+	log_group_name $MOCLOUD_LOG_GROUP
+	log_stream_name $MOCLOUD_LOG_STREAM-#{file[:tag].gsub(".", "-")}
+	auto_create_stream true
+</match>
+	ENDMATCH
+
+end
+
+
+File.open("fluent.conf", "w") do |file|
+	file.write(
+<<-ENDFILE
+		## built-in TCP input
+## $ echo <json> | fluent-cat <tag>
+<source>
+  type forward
+</source>
+
+## File input
+#{sourceString}
+
+# Listen HTTP for monitoring
+# http://localhost:24220/api/plugins
+# http://localhost:24220/api/plugins?type=TYPE
+# http://localhost:24220/api/plugins?tag=MYTAG
+<source>
+  type monitor_agent
+  port 24220
+</source>
+
+# Listen DRb for debug
+<source>
+  type debug_agent
+  bind 127.0.0.1
+  port 24230
+</source>
+
+ENDFILE
+		)
+end
+
+matchString = matchString.shellescape.gsub('\$', '$')
+
+File.open("startfluent.sh", "w") do |file|
+
+	file.write(
+<<-ENDFILE
+#!/bin/bash
+
+/usr/local/bin/fluentd -c /fluent/fluent.conf -i #{matchString}
+ENDFILE
+		)
+end
+
--- startfluent.sh
+++ startfluent.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-/usr/local/bin/fluentd -c /fluent/fluent.conf \
--i "<match modaemon.logs>\ntype cloudwatch_logs\nlog_group_name $MOCLOUD_LOG_GROUP\nlog_stream_name $MOCLOUD_LOG_STREAM-modaemon\nauto_create_stream true\n</match>" \
--i "<match apache.access>\ntype cloudwatch_logs\nlog_group_name $MOCLOUD_LOG_GROUP\nlog_stream_name $MOCLOUD_LOG_STREAM-apache-access\nauto_create_stream true\n</match>" \
--i "<match apache.error>\ntype cloudwatch_logs\nlog_group_name $MOCLOUD_LOG_GROUP\nlog_stream_name $MOCLOUD_LOG_STREAM-apache-errors\nauto_create_stream true\n</match>" \
--i "<match supervisor.log>\ntype cloudwatch_logs\nlog_group_name $MOCLOUD_LOG_GROUP\nlog_stream_name $MOCLOUD_LOG_STREAM-supervisor\nauto_create_stream true\n</match>" \
-
+/usr/local/bin/fluentd -c /fluent/fluent.conf -i \<match\ modaemon.log\>'
+'\	type\ cloudwatch_logs'
+'\	log_group_name\ $MOCLOUD_LOG_GROUP'
+'\	log_stream_name\ $MOCLOUD_LOG_STREAM-modaemon-log'
+'\	auto_create_stream\ true'
+'\</match\>'
+'\<match\ apache.access\>'
+'\	type\ cloudwatch_logs'
+'\	log_group_name\ $MOCLOUD_LOG_GROUP'
+'\	log_stream_name\ $MOCLOUD_LOG_STREAM-apache-access'
+'\	auto_create_stream\ true'
+'\</match\>'
+'\<match\ apache.error\>'
+'\	type\ cloudwatch_logs'
+'\	log_group_name\ $MOCLOUD_LOG_GROUP'
+'\	log_stream_name\ $MOCLOUD_LOG_STREAM-apache-error'
+'\	auto_create_stream\ true'
+'\</match\>'
+'\<match\ supervisor.log\>'
+'\	type\ cloudwatch_logs'
+'\	log_group_name\ $MOCLOUD_LOG_GROUP'
+'\	log_stream_name\ $MOCLOUD_LOG_STREAM-supervisor-log'
+'\	auto_create_stream\ true'
+'\</match\>'
+'
