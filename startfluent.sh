83d1851e4b74b854bd9b5fb7ef82c48de96b745e
David Siaw (via Travis CI) <davidsiaw@gmail.com>
2014-12-12 18:30:25 +0900

    add more configs

--- startfluent.sh
+++ startfluent.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-/usr/local/bin/fluentd -c /fluent/fluent.conf -i "<match modaemon.logs>\ntype cloudwatch_logs\nlog_group_name $MOCLOUD_LOG_GROUP\nlog_stream_name $MOCLOUD_LOG_STREAM-modaemon\nauto_create_stream true\n</match>"
+/usr/local/bin/fluentd -c /fluent/fluent.conf \
+-i "<match modaemon.logs>\ntype cloudwatch_logs\nlog_group_name $MOCLOUD_LOG_GROUP\nlog_stream_name $MOCLOUD_LOG_STREAM-modaemon\nauto_create_stream true\n</match>" \
+-i "<match apache.access>\ntype cloudwatch_logs\nlog_group_name $MOCLOUD_LOG_GROUP\nlog_stream_name $MOCLOUD_LOG_STREAM-apache-access\nauto_create_stream true\n</match>" \
+-i "<match apache.error>\ntype cloudwatch_logs\nlog_group_name $MOCLOUD_LOG_GROUP\nlog_stream_name $MOCLOUD_LOG_STREAM-apache-errors\nauto_create_stream true\n</match>" \
+-i "<match supervisor.log>\ntype cloudwatch_logs\nlog_group_name $MOCLOUD_LOG_GROUP\nlog_stream_name $MOCLOUD_LOG_STREAM-supervisor\nauto_create_stream true\n</match>" \
+
